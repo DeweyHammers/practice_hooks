dad179019d16d555e12326e8e2ec92f7e705cb06
dewey <hammersdrichard@gmail.com>
2021-11-04 15:05:02 -0700

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import StarRating from "./components/StarRating";
+import Congratulation from "./components/Congratulation";
+import Github from "./components/Github";
 
 const App = () => {
   return (
     <div>
       <StarRating totalStars={5} />
+      <Congratulation />
+      <Github />
     </div>
   );
 };
---
+++ src/components/Github.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+const Github = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users")
+      .then((res) => res.json())
+      .then(setData);
+  }, []);
+
+  if (data) {
+    return (
+      <div>
+        <ul>
+          {data.map((user) => (
+            <li key={user.id}>{user.login}</li>
+          ))}
+        </ul>
+        <button onClick={() => setData([])}>Remove Data</button>
+      </div>
+    );
+  }
+
+  return <p>No Users</p>;
+};
+
+export default Github;
