9e6dcd0deec20b5ec3c7bb0e1ec60aa232ed31c2
dewey <hammersdrichard@gmail.com>
2021-11-04 14:40:42 -0700

    first commit

---
+++ src/components/StarRating.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Star from "./Star";
+
+const createArray = (length) => [...Array(length)];
+
+const StarRating = ({ totalStars = 5 }) => {
+  const [selectedStars, setSelectedStars] = useState(0);
+
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars}
+      </p>
+    </>
+  );
+};
+
+export default StarRating;
